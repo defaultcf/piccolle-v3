6194ef9603ae3b3e0a98e6b16d1485b4b106a131
i544c <leo@i544c.me>
2017-07-17 03:27:52 +0900

    Modify view

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ ruby '2.4.1'
 
 gem 'nokogiri', '1.8.0'
 gem 'sinatra', '2.0.0'
+gem 'sinatra-contrib', '2.0.0'
--- main.rb
+++ main.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
+require 'sinatra/json'
 require_relative "scrape"
 
 set :bind, '0.0.0.0'
 set :port, 8080
+set :public_folder, File.dirname(__FILE__) + '/static'
+
+get '/' do
+    erb :index
+end
 
 get '/search' do
     s = Scrape.new
-    s.search(params['str'], params['n'].to_i).to_s
+    json s.search(params['str'], params['n'].to_i)
 end
