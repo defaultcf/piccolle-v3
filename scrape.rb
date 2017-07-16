d36e409b2c06aa7690a0f10bcc010267adf738cc
i544c <leo@i544c.me>
2017-07-17 01:48:47 +0900

    Modify for 404, 503

--- scrape.rb
+++ scrape.rb
@@ -6,25 +6,41 @@ require 'kconv'
 class Scrape
     def search(str)
         url = 'http://find.2ch.sc/?STR=' + str.gsub(/\s/, '+')
-        html = open(url, 'r:binary').read
-        doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
+        begin
+            doc = open_html(url)
+        rescue => e
+            p e
+            return
+        end
         doc.xpath('//dt').each do |node|
             a = node.css('a')
             if !a.empty? then
                 thre = a.attribute('href').value.to_s
                 p getimg(thre)
+                sleep(1)
             end
         end
     end
 
     def getimg(url)
-        html = open(url, 'r:binary').read
-        doc = Nokogiri::HTML(html.toutf8, nil, 'utf-8')
+        begin
+            doc = open_html(url)
+        rescue => e
+            p e
+            p url
+            return
+        end
         imgs = []
         doc.xpath('//dd').each do |node|
             img = node.text.scan(/ttp:\/\/\S+\.(?:jpg|png)/)
             imgs += img
         end
         return imgs
+    end
+
+    private
+    def open_html(url)
+        html = open(url, 'r:binary').read
+        return Nokogiri::HTML(html.toutf8, nil, 'utf-8')
     end
 end
